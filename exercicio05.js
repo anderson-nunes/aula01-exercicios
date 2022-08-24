18d4bd1527f1f4b061772af67ebfca4806965931
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-08-24 18:18:20 -0300

    adicionando alternativa de exercício usando operador ternário

--- exercicio05.js
+++ exercicio05.js
@@ -4,24 +4,41 @@ const diaDasemana = leitor.questionInt(
   "Digite um numero de 1 a 7 que corresponde ao dia da semana que voce deseja: "
 );
 
-let mensagem
+let error = "Este valor não corresponde a um dia da semana";
 
-if (diaDasemana == 1) {
-  mensagem = "Domingo";
-} else if (diaDasemana == 2) {
-  mensagem = "Segunda-feira";
-} else if (diaDasemana == 3) {
-  mensagem = "Terça-feira";
-} else if (diaDasemana == 4) {
-  mensagem = "Quarta-feira";
-} else if (diaDasemana == 5) {
-  mensagem = "Quinta-feira";
-} else if (diaDasemana == 6) {
-  mensagem = "Sexta-feira";
-} else if (diaDasemana == 7) {
-  mensagem = "Sábado";
-} else {
-  mensagem ="Este valor não corresponde a um dia da semana";
-}
+mensagem =
+  diaDasemana == 1
+    ? "Domingo"
+    : diaDasemana == 2
+    ? "Segunda-feira"
+    : diaDasemana == 3
+    ? "Terça-feira"
+    : diaDasemana == 4
+    ? "Quarta-feira"
+    : diaDasemana == 5
+    ? "Quinta-feira"
+    : diaDasemana == 6
+    ? "Sexta-feira"
+    : diaDasemana == 7
+    ? " Sábado"
+    : error;
+
+// if (diaDasemana == 1) {
+//   mensagem = "Domingo";
+// } else if (diaDasemana == 2) {
+//   mensagem = "Segunda-feira";
+// } else if (diaDasemana == 3) {
+//   mensagem = "Terça-feira";
+// } else if (diaDasemana == 4) {
+//   mensagem = "Quarta-feira";
+// } else if (diaDasemana == 5) {
+//   mensagem = "Quinta-feira";
+// } else if (diaDasemana == 6) {
+//   mensagem = "Sexta-feira";
+// } else if (diaDasemana == 7) {
+//   mensagem = "Sábado";
+// } else {
+//   mensagem ="Este valor não corresponde a um dia da semana";
+// }
 
 console.log(mensagem);
---
+++ exercicio06.js
@@ -0,0 +1,10 @@
+const leitor = require("readline-sync");
+
+const tamanho = leitor.questionFloat("Digite o tamanho em metros quadrados da área a ser pintada: ");
+
+const litros = tamanho / 6
+const latas = litros / 18
+const preco = latas * 80
+const galoes = litros / 3.6
+
+
