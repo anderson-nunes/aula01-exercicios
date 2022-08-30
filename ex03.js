7a4458df84e05f462e54778a1b6b946bf410b657
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-08-30 14:55:31 -0300

    Aula 01 - Exercício 03

--- ex03.js
+++ ex03.js
@@ -2,10 +2,10 @@ const leitor = require("readline-sync");
 
 const genero = leitor.question("Qual é o seu gênero? ");
 
-let altura = leitor.question("Qual é a sua altura?  ");
+let altura = leitor.question("Qual é a sua altura? ");
 
 let pesoIdealHomem = 72.7 * altura - 58;
 let pesoIdealMulher = 62.1 * altura - 44.7;
 
 console.log(pesoIdealHomem);
-console.log(pesoIdealMulher);
+// console.log(pesoIdealMulher);
