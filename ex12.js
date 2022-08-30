61382501d11b0ca214740100d4e82b2761849f6f
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-08-30 15:09:11 -0300

    Aula 02 - Exercício 12

--- ex12.js
+++ ex12.js
@@ -1,6 +1,6 @@
 const leitor = require("readline-sync");
 
-let numero = leitor.questionInt("Digite um numero: ");
+let numero = leitor.questionInt("Digite um número: ");
 
 for (let i = numero; i <= 1000; i++) {
   console.log(numero);
