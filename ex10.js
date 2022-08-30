1508dc3a6f0d083e6557cad91913c3c7ae4af735
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-08-30 15:02:51 -0300

    Aula 01 - Exercício 10

--- ex10.js
+++ ex10.js
@@ -1,7 +1,7 @@
 const leitor = require("readline-sync");
 
 const codigo = leitor.questionInt(
-  "Digite o codigo do produto que deseja comprar: "
+  "Digite o código do produto que deseja comprar: "
 );
 
 const quantidade = leitor.questionInt(
