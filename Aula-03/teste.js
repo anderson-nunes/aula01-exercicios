27fcb9d9af2df0f0500cac143e59b06d796b7af7
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-09-13 09:34:10 -0300

    Atualizando pastas e exercícios

---
+++ Aula-03/teste.js
@@ -0,0 +1,33 @@
+const leitor = require("readline-sync");
+
+let array1 = [];
+let array2 = [];
+
+for (let i = 0; i < 5; i++) {
+  array1.push(Math.floor(Math.random() * 8));
+  array2.push(Math.floor(Math.random() * 8));
+}
+
+let array3 = [];
+
+for (let i = 0; i < 5; i++) {
+  array3[i] = array1[i] - array2[i];
+}
+
+let array4 = [];
+
+for (let i = 0; i < 5; i++) {
+  array4[i] = array1[i] + array2[i];
+}
+
+let array5 = [];
+
+for (let i = 0; i < 5; i++) {
+  array5[i] = array1[i] * array2[i];
+}
+
+console.log(array1);
+console.log(array2);
+console.log(array3);
+console.log(array4);
+console.log(array5);
